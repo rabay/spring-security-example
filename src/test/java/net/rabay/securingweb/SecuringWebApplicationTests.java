016a99be383eea55e193ca0abd5d6fb56d42bb13
Victor Rabay <>
2024-05-18 15:34:24 -0300

    Implements Spring Security

--- src/test/java/net/rabay/securingweb/SecuringWebApplicationTests.java
+++ src/test/java/net/rabay/securingweb/SecuringWebApplicationTests.java
@@ -1,10 +1,10 @@
-package net.rabay.login;
+package net.rabay.securingweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginApplicationTests {
+class SecuringWebApplicationTests {
 
 	@Test
 	void contextLoads() {
