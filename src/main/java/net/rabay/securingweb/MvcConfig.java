8ce56005b558bd5802793368c21d96b7c839685f
Victor Rabay <>
2024-08-01 21:14:55 -0300

    Update code

--- src/main/java/net/rabay/securingweb/MvcConfig.java
+++ src/main/java/net/rabay/securingweb/MvcConfig.java
@@ -1,13 +1,15 @@
 package net.rabay.securingweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-	public void addViewControllers(ViewControllerRegistry registry) {
+	@Override
+	public void addViewControllers(@NonNull ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
--- src/main/java/net/rabay/securingweb/WebSecurityConfig.java
+++ src/main/java/net/rabay/securingweb/WebSecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,25 +20,23 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-			)
-			.formLogin((form) -> form
-				.loginPage("/login")
-				.permitAll()
-			)
-			.logout((logout) -> logout.permitAll());
+				.authorizeHttpRequests(requests -> requests
+						.requestMatchers("/", "/home").permitAll()
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.permitAll())
+				.logout(LogoutConfigurer::permitAll);
 
 		return http.build();
 	}
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		UserDetails user = User.builder()
 				.username("user")
-				.password("password")
+				.password(passwordEncoder.encode("password"))
 				.roles("USER")
 				.build();
 
